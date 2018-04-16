44ac9716bb172ecf4fb4f53ece4a387ed1fe63b7
Leodecio Braz <segundolbs@gmail.com>
2018-04-16 17:13:54 -0300

    lista 6

--- bibliotecas/knn.py
+++ bibliotecas/knn.py
@@ -31,7 +31,7 @@ class KNNClassifier:
     
     def predict(self,X_test):
         y_pred = []
-        for i in range(0,X_test.shape[0]):
+        for i in range(X_test.shape[0]):
             idx_sort = self.get_neighbors(self.X, X_test[i],self.k)
             output_values = self.y[idx_sort]
             counts = np.unique(output_values, return_counts=True)
@@ -41,27 +41,32 @@ class KNNClassifier:
 
 
 class KNNRegressor:
-	k = None
-	metric = None
-	p = None
-	def __init__(self, k=5, metric='minkowski', p=2):
-		self.k = k
-		self.metric = metric
-		self.p = p
+    k = 0
+    metric = None
+    p = None
+    X = None
+    y = None
+    def __init__(self, k=5, metric='minkowski', p=2):
+        self.k = k
+        self.metric = metric
+        self.p = p
 
+    def get_neighbors(self,X_train, test_row,k):
+        if(self.metric == 'minkowski'):
+            distances = dm.minkowski_distance(X_train,test_row, self.p)
+        elif(self.metric == 'euclidian'):
+            distances = dm.euclidean_distance(X_train,test_row)
+        elif(self.metric == 'euclidian'):
+            distances = dm.manhattan_distance(X_train,test_row)
+        elif(self.metric == 'chebyshev'):
+            distances = dm.chebyshev_distance(X_train,test_row)
+        idx_sort = np.argsort(distances)
+        return idx_sort[1:k+1]
 
-	def get_neighbors(X_train, test_row, k):
-		if(self.metric == 'minkowski'):
-			distances = dm.minkowski_distance(X_train,test_row, p)
-		elif(self.metric == 'chebyshev'):
-			distances = dm.chebyshev_distance(X_train,test_row)
-		#distances = euclidean_distances(X_train, test_row)
-		idx_sort = np.argsort(distances)
-		return idx_sort[1:k+1]
-
-	def predict_regression(X, y, test_row, k= k):
-		idx_sort = get_neighbors(X, test_row, k)
-		output_values = y[idx_sort]
-		prediction = np.sum(output_values) / output_values.shape[0]
-		print('idx_sort:{}, output_values:{}, prediction:{}'.format(idx_sort, output_values, prediction))
-		return prediction
+    def predict(self, X_test):
+        y_pred = []
+        for i in range(X_teste.shape[0]):
+            idx_sort = get_neighbors(self.X, X_test[i], self.k)
+            output_values = self.y[idx_sort]
+            y_pred.append(np.sum(output_values) / output_values.shape[0])
+        return y_pred
