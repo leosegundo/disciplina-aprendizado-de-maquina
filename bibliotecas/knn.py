0ce6fa7f6598cd693b2f4f006347cc9dd1c2faf1
Leodecio Braz <segundolbs@gmail.com>
2018-04-16 15:41:54 -0300

    lista 6

--- bibliotecas/knn.py
+++ bibliotecas/knn.py
@@ -2,33 +2,42 @@ import dist_metrics as dm
 import numpy as np
 
 class KNNClassifier:
-	k = 0
-	metric = None
-	p = None
-	def __init__(self, k=5, metric='minkowski', p=2):
-		self.k = k
-		self.metric = metric
-		self.p = p
+    k = 0
+    metric = None
+    p = None
+    X = None
+    y = None
+    
+    def __init__(self, k=5, metric='minkowski', p=2):
+        self.k = k
+        self.metric = metric
+        self.p = p
 
-	def get_neighbors(self,X_train, test_row):
-		k = self.k
-		if(self.metric == 'minkowski'):
-			distances = dm.minkowski_distance(X_train,test_row, self.p)
-		elif(self.metric == 'chebyshev'):
-			distances = dm.chebyshev_distance(X_train,test_row)
-		#distances = euclidean_distances(X_train, test_row)
-		idx_sort = np.argsort(distances)
-		return idx_sort[1:k+1]
+    def get_neighbors(self,X_train, test_row,k):
+        if(self.metric == 'minkowski'):
+            distances = dm.minkowski_distance(X_train,test_row, self.p)
+        elif(self.metric == 'euclidian'):
+            distances = dm.euclidean_distance(X_train,test_row)
+        elif(self.metric == 'euclidian'):
+            distances = dm.manhattan_distance(X_train,test_row)
+        elif(self.metric == 'chebyshev'):
+            distances = dm.chebyshev_distance(X_train,test_row)
+        idx_sort = np.argsort(distances)
+        return idx_sort[1:k+1]
 
-	def predict_classification(self,X, y, test_row, k = k):
-		idx_sort = self.get_neighbors(X, test_row)
-		print(idx_sort)
-		output_values = y[idx_sort]
-		counts = np.unique(output_values, return_counts=True)
-		idx_max = np.argmax(counts[1])
-		prediction = counts[0][idx_max]
-		print('idx_sort:{}, output_values:{}, prediction:{}'.format(idx_sort, output_values, prediction))
-		return prediction
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+    
+    def predict(self,X_test):
+        y_pred = []
+        for i in range(0,X_test.shape[0]):
+            idx_sort = self.get_neighbors(self.X, X_test[i],self.k)
+            output_values = self.y[idx_sort]
+            counts = np.unique(output_values, return_counts=True)
+            idx_max = np.argmax(counts[1])
+            y_pred.append(counts[0][idx_max])
+        return y_pred
 
 
 class KNNRegressor:
